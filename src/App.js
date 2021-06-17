01e87dc012f2281474227bd5eac34b80386a5d67
raityupiyo <42093052+piyoraik@users.noreply.github.com>
2021-06-17 16:05:06 +0900

    Cognito

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { createTodo } from "./graphql/mutations";
 import { listTodos } from "./graphql/queries";
 
 import awsExports from "./aws-exports";
+import { withAuthenticator } from "@aws-amplify/ui-react";
 Amplify.configure(awsExports);
 
 const initialState = { name: "", description: "" };
@@ -96,4 +97,4 @@ const styles = {
 	},
 };
 
-export default App;
+export default withAuthenticator(App);
